7c7a6aaa58e5cc8fc55b7c6964c14464a5178605
klay964 <dody200039@gmail.com>
2020-07-09 01:21:33 +0300

    Launch

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <p className="text">
           {" "}
           About Us <br></br>A group of UOITC students who are willing to help
-          their Society during the hard time facing COVID-19 virus, So thier
+          thier Society during the hard time facing COVID-19 virus, So thier
           main goal is use technonolgy to help people either by rise awareness
           or donation
         </p>
